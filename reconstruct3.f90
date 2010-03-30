6d5cb630562e8b66c44925a8bf8e3a258b92a9b1
rhys <rhys@b505329a-af50-4d4e-8b30-0926ca80c829>
2010-03-30 20:51:33 +0000

    case 2 draft
    git-svn-id: https://svn.ices.utexas.edu/repos/rhys/codefour/trunk@1012 b505329a-af50-4d4e-8b30-0926ca80c829

--- reconstruct3.f90
+++ reconstruct3.f90
@@ -57,28 +57,22 @@ SUBROUTINE reconstruct (ur, u, n, bias)
   DO i = 1, n+1, 1
    v0 = v(i)
    vp = v(i+1)
-   vpp = v(i+2)
    vm = v(i-1)
-   vmm = v(i-2)
-! The three reconstructed values at x(i-1/2)
-! Slightly different formulas than (2.11), (2.12), (2.13)
-   urloc(1) = a(2,1) * vmm + a(2,2) * vm + a(2,3) * v0
-   urloc(2) = a(3,1) * vm + a(3,2) * v0 + a(3,3) * vp
-   urloc(3) = a(1,3) * v0 + a(1,2) * vp + a(1,1) * vpp
-! Smoothness indicators, formula (2.17)
-   beta(1) = b(1) * (vmm - 2.d0 * vm + v0)**2 + b(2) *( vmm - 4.d0 * vm + 3.d0 * v0)**2
-   beta(2) = b(1) * (vm - 2.d0 * v0 + vp)**2 + b(2) * (vm - vp)**2
-   beta(3) = b(1) * (v0 - 2.d0 * vp + vpp)**2 + b(2) * (3.d0 * v0 - 4.d0 * vp + vpp)**2
-! Compute nonlinear weights (2.10)
-   wt(1) = gam(3) / ((eps + beta(1))**2)
-   wt(2) = gam(2) / ((eps + beta(2))**2)
-   wt(3) = gam(1) / ((eps + beta(3))**2)
-   wtsumi = 1.d0 / (wt(1) + wt(2) + wt(3))
+! The reconstructed values at x(i-1/2) per p'(j), p'(j+1) from bottom of p205
+! Note mistake in the p'j formula, i.e. (x-x).
+   urloc(1) = a(2,1) * vm + a(2,2) * v0
+   urloc(2) = a(1,2) * v0 + a(1,1) * vp
+! Smoothness indicators from p206 just above equation 3.16
+   beta(1) = (v0 - vm)**2
+   beta(2) = (vp - v0)**2
+! Compute nonlinear weights (3.17a)
+   wt(1) = 1.0d0 / ((eps + beta(1))**2)
+   wt(2) = 0.5d0 / ((eps + beta(2))**2)
+   wtsumi = 1.d0 / (wt(1) + wt(2))
    w(1) = wt(1) * wtsumi
    w(2) = wt(2) * wtsumi
-   w(3) = wt(3) * wtsumi
-! Finally reconstruct! Formula (2.16)
-   ur(i-1) = w(1) * urloc(1) + w(2) * urloc(2) + w(3) * urloc(3)
+! Finally reconstruct, formula (3.16)
+   ur(i) = w(1) * urloc(1) + w(2) * urloc(2)
   END DO
  END IF
 END SUBROUTINE reconstruct
