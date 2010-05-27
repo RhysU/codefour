1c7d9a0103557043ee2447551200134ae1959c51
rhys <rhys@b505329a-af50-4d4e-8b30-0926ca80c829>
2010-05-27 18:19:31 +0000

    Update plotted for HDF5 + Octave
    git-svn-id: https://svn.ices.utexas.edu/repos/rhys/codefour/trunk@1049 b505329a-af50-4d4e-8b30-0926ca80c829

--- plotter.m
+++ plotter.m
@@ -1,10 +1,18 @@
+#!/usr/bin/env octave
 
-a=dir('sol*.txt');
-load x.txt
+if nargin < 1
+  printf("No HDF5 filename specified")
+  exit(1)
+endif
+
 set(gca,'FontSize',18)
+arg_list = argv()
+load("-hdf5", arg_list{1})
 
-for i = 1:length(a)
-  u = load(a(i).name);
-  plot(x,u,'ko-')
+for i = 1:size(u,2)
+  plot(x,u(:,i),'ko-')
+  title(sprintf("t = %g", t(i)))
+  xlabel("x")
+  ylabel("u(x,t)")
   drawnow
 end
