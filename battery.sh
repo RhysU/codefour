c376a230a533f32e99492366503e2495dd99cb71
rhys <rhys@b505329a-af50-4d4e-8b30-0926ca80c829>
2010-05-28 17:46:54 +0000

    Remove weno3,5 run
    git-svn-id: https://svn.ices.utexas.edu/repos/rhys/codefour/trunk@1068 b505329a-af50-4d4e-8b30-0926ca80c829

--- battery.sh
+++ battery.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Run a battery of test cases in parallel
-for code in weno32 weno54 weno3 weno5
+for code in weno32 weno54
 do
     echo $code spawning...
     for resolution in 25 50 100 125 150 175 200 250 300 350
