d9bb62fa7c1076e4112c352319401eb19f3c1aa0
rhys <rhys@b505329a-af50-4d4e-8b30-0926ca80c829>
2010-05-31 16:25:47 +0000

    Formatting cleanups
    git-svn-id: https://svn.ices.utexas.edu/repos/rhys/codefour/trunk@1075 b505329a-af50-4d4e-8b30-0926ca80c829

--- assorted.F90
+++ assorted.F90
@@ -1,15 +1,20 @@
 ! $HeadURL$
 ! $Id$
+
+!> Print a double precision one dimensional array to a flat text file.
+!!
+!! @param u   Data to be output
+!! @param n   Number of data points to write
+!! @param str Filename to use
 SUBROUTINE printdble (u, n, str)
- USE doublePrecision
- IMPLICIT NONE
- INTEGER, INTENT(IN) :: n
- REAL(KIND = dp), INTENT(IN) :: u(0:n)
- CHARACTER (LEN = *), INTENT(IN) :: str
- INTEGER :: i
- OPEN (2, FILE = TRIM(str), STATUS = 'UNKNOWN')
- DO i = 0, n, 1
-  WRITE (2, fmt = '(E24.16)') u(i)
- END DO
- CLOSE (2)
+  USE doublePrecision
+  IMPLICIT NONE
+  INTEGER, INTENT(IN) :: n
+  REAL(KIND = dp), INTENT(IN) :: u(0:n)
+  CHARACTER(LEN = *), INTENT(IN) :: str
+  INTEGER :: i
+
+  OPEN  (2, FILE = TRIM(str), STATUS = 'UNKNOWN')
+  WRITE (2, fmt = '(E24.16)') (u(i),i=0,n,1)
+  CLOSE (2)
 END SUBROUTINE printdble
