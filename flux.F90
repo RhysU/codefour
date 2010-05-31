e25874faa439e8bb26754276aabcb845ee2210f7
rhys <rhys@b505329a-af50-4d4e-8b30-0926ca80c829>
2010-05-31 16:20:16 +0000

    Reformat and doxygen flux.F90
    git-svn-id: https://svn.ices.utexas.edu/repos/rhys/codefour/trunk@1073 b505329a-af50-4d4e-8b30-0926ca80c829

--- flux.F90
+++ flux.F90
@@ -1,31 +1,31 @@
 ! $HeadURL$
 ! $Id$
+
+!> Compute the Lax-Friedrichs flux
+!! \f$\hat{f}^{\text{LF}}\left(u^{-},u^{+}\right)\f$ given values of
+!! \f$u\f$.  See section 3.1 of Shu's 2009 SIAM Review paper
+!! or section 2 of Liu, Sher, and Chan's 1994 JCP paper for more details.
+!!
+!! @param fp
+!! @param fm
+!! @param u  \f$u\f$
+!! @param n  Grid size
 SUBROUTINE flux (fp, fm, u, n)
-! This function takes values of u and computes the two fluxes
- USE doublePrecision
- IMPLICIT NONE
- INTEGER, INTENT(IN) :: n
- REAL(KIND = dp), INTENT(IN) :: u(0:n)
- REAL(KIND = dp), INTENT(OUT) :: fp(0:n), fm(0:n)
- REAL(KIND = dp) :: alpha
- INTEGER :: i
+  USE doublePrecision
+  IMPLICIT NONE
+  INTEGER, INTENT(IN)          :: n
+  REAL(KIND = dp), INTENT(IN)  :: u(0:n)
+  REAL(KIND = dp), INTENT(OUT) :: fp(0:n), fm(0:n)
+  REAL(KIND = dp)              :: alpha
 
- IF (.FALSE.) THEN
-! The scalar advection equation
-  DO i = 0, n, 1
-   alpha = 1.d0
-   fp(i) = 0.5_dp * (u(i) + alpha * u(i))
-   fm(i) = 0.5_dp * (u(i) - alpha * u(i))
-  END DO
- END IF
+! Euler equation where f(u) := u**2/2
+  alpha = MAXVAL(ABS(u))
+  fp    = 0.5_dp * (0.5_dp*u**2 + alpha*u)
+  fm    = 0.5_dp * (0.5_dp*u**2 - alpha*u)
 
- IF (.TRUE.) THEN
-! Eulers' equation
-  DO i = 0, n, 1
-   alpha = MAXVAL(ABS(u))
-   fp(i) = 0.5_dp * (0.5_dp * u(i)**2 + alpha * u(i))
-   fm(i) = 0.5_dp * (0.5_dp * u(i)**2 - alpha * u(i))
-  END DO
- END IF
+! The scalar advection equation where f(u) := u
+! alpha = 1.d0
+! fp    = 0.5_dp * (u + alpha*u)
+! fm    = 0.5_dp * (u - alpha*u)
 
 END SUBROUTINE flux
